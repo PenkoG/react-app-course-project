812b4a5370b2ab4d1f4615d8b2c22b72a291b995
PenkoG <81184142+PenkoG@users.noreply.github.com>
2021-11-27 13:29:02 +0200

    Creating Header
    Implementing the header and adding css to it.
    Installing react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
+import Header from "./components/Header";
 import RegisterPage from './components/Register/RegisterPage';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <RegisterPage />
 
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import { NavLink } from "react-router-dom";
+
+export default function Header() {
+    return (
+        <header className="header-container">
+
+            <div className="logo-container">
+                <NavLink to="/home">
+                    <img src="./logo.png" alt="" width="200px" />
+                </NavLink>
+            </div>
+
+            <nav className="nav-container">
+                <ul className="nav-ul" >
+                    <NavLink className="nav-link" activeClassName="active-navlink" to="/home" >HOME</NavLink>
+                    <NavLink className="nav-link" activeClassName="active-navlink" to="/my-list" >MY LIST</NavLink>
+                    <NavLink className="nav-link" activeClassName="active-navlink" to="/login" >LOGIN</NavLink>
+                    <NavLink className="nav-link" activeClassName="active-navlink" to="/register" >REGISTER</NavLink>
+                </ul>
+            </nav>
+
+            <div className="right-container">
+                <div className="search-icon">
+                    <i className="fas fa-search"></i>
+                </div>
+
+                <div className="login-button-container">
+                    <NavLink to="/login">LogIn</NavLink>
+                </div>
+            </div>
+
+            <div className="top-shadow">
+                <img src="topGradient.png" alt="image" />
+            </div>
+
+        </header >
+    )
+}
