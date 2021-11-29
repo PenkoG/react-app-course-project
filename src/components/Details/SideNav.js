dea53f98724d6574ffcc269d2e411a128f09ab41
PenkoG <81184142+PenkoG@users.noreply.github.com>
2021-11-29 08:21:04 +0200

    Created SideNav component

---
+++ src/components/Details/SideNav.js
@@ -0,0 +1,19 @@
+import { NavLink } from "react-router-dom";
+
+export default function SideNav() {
+    return (
+        <div className="side-nav-container">
+            <ul className="side-nav">
+                <li className="side-nav-trailer" >
+                    <NavLink to="/details" className="side-nav-trailer">trailer</NavLink>
+                </li>
+                <li className="side-nav-cast" >
+                    <NavLink to="/details/cast" className="side-nav-cast">cast</NavLink>
+                </li>
+                <li className="side-nav-overview" >
+                    <NavLink to="/details/overview" className="side-nav-overview">overview</NavLink>
+                </li>
+            </ul>
+        </div>
+    )
+}
