14bded617f41663208c7f9e53beaf2b9e7721549
PenkoG <81184142+PenkoG@users.noreply.github.com>
2021-11-29 09:10:22 +0200

    OverviewPage html update

--- src/components/Details/OverviewPage.js
+++ src/components/Details/OverviewPage.js
@@ -3,24 +3,20 @@ import SideNav from "./SideNav"
 export default function OverviewPage() {
     return (
         <div className="details-overview-container">
-            <h1>OVERVIEW</h1>
-
             <SideNav></SideNav>
 
             <div className="actor-image-container">
                 <img src="" className="actor-image" alt="" />
             </div>
 
-            <div className="actor-info-container">
-                <h1 className="actor-name">Actor Name</h1>
-                <span className="role">as Cletus Casedy | Carnage</span>
-                <p className="actor-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi ea cupiditate provident explicabo exercitationem inventore quae sint saepe tenetur placeat, officia eius aliquid sit ipsum facere vitae dicta debitis expedita eveniet optio a earum repellendus soluta! Illo libero repudiandae ipsum obcaecati eum! Minima ab delectus nam dolores corporis doloremque iure.</p>
+            <div className="movie-info-container">
+                <h1 className="movie-name">Lorem Ipsum Dolar</h1>
+                <p className="movie-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi ea cupiditate provident explicabo exercitationem inventore quae sint saepe tenetur placeat, officia eius aliquid sit ipsum facere vitae dicta debitis expedita eveniet optio a earum repellendus soluta! Illo libero repudiandae ipsum obcaecati eum! Minima ab delectus nam dolores corporis doloremque iure.</p>
+                <h1 className="movie-name">Lorem Ipsum Dolar</h1>
+                <p className="movie-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi ea cupiditate provident explicabo exercitationem inventore quae sint saepe tenetur placeat, officia eius aliquid sit ipsum facere vitae dicta debitis expedita eveniet optio a earum repellendus soluta! Illo libero repudiandae ipsum obcaecati eum! Minima ab delectus nam dolores corporis doloremque iure.</p>
             </div>
 
-            <div className="actor-scroll">
-                <img src="" alt="" />
-                <img src="" alt="" />
-                <img src="" alt="" />
+            <div className="cast-bgr">
             </div>
         </div>
     )
