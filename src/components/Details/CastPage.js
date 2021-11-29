09aef173f19f2dcd577357693d6c265028c04b96
PenkoG <81184142+PenkoG@users.noreply.github.com>
2021-11-29 08:14:28 +0200

    Update CastPage
    html corrections

--- src/components/Details/CastPage.js
+++ src/components/Details/CastPage.js
@@ -2,6 +2,23 @@ export default function CastPage() {
     return (
         <div className="details-trailer-container">
             <h1>Cast</h1>
+
+            <div className="actor-image-container">
+                <img src="" className="actor-image" alt="" />
+            </div>
+
+            <div className="actor-info-container">
+                <h1 className="actor-name">Actor Name</h1>
+                <span className="role">as Cletus Casedy | Carnage</span>
+                <p className="actor-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi ea cupiditate provident explicabo exercitationem inventore quae sint saepe tenetur placeat, officia eius aliquid sit ipsum facere vitae dicta debitis expedita eveniet optio a earum repellendus soluta! Illo libero repudiandae ipsum obcaecati eum! Minima ab delectus nam dolores corporis doloremque iure.</p>
+            </div>
+
+            <div className="actor-scroll">
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+            </div>
+
         </div>
     )
 }
