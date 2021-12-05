008ce1b9d251f58a0886663863cdd3e08a20da91
PenkoG <81184142+PenkoG@users.noreply.github.com>
2021-12-05 16:37:42 +0200

    page 404 added navigation link to home

--- client/src/components/404/404.js
+++ client/src/components/404/404.js
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Page404() {
+
     return (
         < div className="page404-container" >
-            <button className="main-button">GO TO HOME</button>
+            <Link to="/">
+                <button className="main-button">GO TO HOME</button>
+            </Link>
             <div className="page4040-image-container">
                 <img className="register-bgr-img" src="404.png" alt="" />
             </div>
