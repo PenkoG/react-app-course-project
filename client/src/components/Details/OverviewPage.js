5a435bb4584c1e4c4ec7455f54f826038cecd8d4
PenkoG <81184142+PenkoG@users.noreply.github.com>
2021-12-05 16:38:39 +0200

    overviewPage functionality  added

--- client/src/components/Details/OverviewPage.js
+++ client/src/components/Details/OverviewPage.js
@@ -1,22 +1,50 @@
 import SideNav from "./SideNav"
+import UpdatePage from "../Update/UpdatePage";
+
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from "axios";
 
 export default function OverviewPage() {
+    const [movieData, setMovieData] = useState({});
+    const navigate = useNavigate();
+
+    let id = sessionStorage.getItem("movie-id")
+    let updateLink = `/update/${id}`
+
+
+    useEffect(() => {
+        axios.get(`http://localhost:8800/api/movies/find/${id}`)
+            .then(res => {
+                setMovieData(res.data);
+            })
+    }, []);
+    console.log(movieData);
+
+    function updateMovieHandler() {
+        console.log('Here');
+        navigate(updateLink);
+    }
+
     return (
         <div className="details-overview-container">
             <SideNav></SideNav>
 
             <div className="movie-image-container">
-                <img src="https://m.media-amazon.com/images/M/MV5BN2FjNmEyNWMtYzM0ZS00NjIyLTg5YzYtYThlMGVjNzE1OGViXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg" className="movie-image" alt="" />
+                <img src={movieData.imgUrl} className="movie-image" alt="" />
             </div>
 
             <div className="movie-info-container">
-                <h1 className="movie-title">Dune</h1>
-                <span className="movie-year"> year: 2021</span>
+                <h1 className="movie-title">{movieData.title}</h1>
+                <span className="movie-year"> year: {movieData.year}</span>
                 <span className="pipe">|</span>
-                <span className="movie-genre"> Sci-fi </span>
+                <span className="movie-genre"> {movieData.genre} </span>
                 <span className="pipe">|</span>
-                <span className="movie-length"> 140m </span>
-                <p className="movie-description-overview">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi ea cupiditate provident explicabo exercitationem inventore quae sint saepe tenetur placeat, officia eius aliquid sit ipsum facere vitae dicta debitis expedita eveniet optio a earum repellendus soluta! Illo libero repudiandae ipsum obcaecati eum! Minima ab delectus nam dolores corporis doloremque iure.</p>
+                <span className="movie-length"> {movieData.duration} </span>
+                <p className="movie-description-overview">{movieData.description}</p>
+                <Link to={'/update/' + movieData._id}>
+                    <button className="login-button-container" onClick={updateMovieHandler} >UPDATE</button>
+                </Link>
             </div>
 
             <div className="overview-bgr-img-container" scr="OverviewBGR.jpg" alt="">
