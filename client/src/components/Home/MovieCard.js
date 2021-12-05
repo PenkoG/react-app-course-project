4422753a35fd28c652a9bc64268d48bcccd7e47c
PenkoG <81184142+PenkoG@users.noreply.github.com>
2021-12-05 16:41:50 +0200

    MovieCard component added

---
+++ client/src/components/Home/MovieCard.js
@@ -0,0 +1,33 @@
+import { useNavigate } from "react-router";
+
+export default function MovieCard({ movie }) {
+    let navigate = useNavigate()
+
+    function onClickHandler() {
+        sessionStorage.setItem("movie-id", movie._id);
+        sessionStorage.setItem("movie-info", movie.title)
+        navigate(`details/${movie._id}`);
+    }
+
+    return (
+        <div className="movie-card-container">
+
+            <div className="image-container">
+                <img src={movie.imgUrl} alt="img" />
+            </div>
+
+            <h2 className="movie-headline">{movie.title}</h2>
+            <p className="movie-description">{movie.description.substring(0, 205)}</p>
+
+            <div className="rating-container">
+                <p>Rating:</p>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+                <i className="far fa-star"></i>
+            </div>
+            <button onClick={onClickHandler}>Details</button>
+        </div>
+    )
+}
