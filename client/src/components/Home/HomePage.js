e20bc6d8c2c652de6a0f10fb6b84ebcc2481fd32
PenkoG <81184142+PenkoG@users.noreply.github.com>
2021-12-05 16:41:27 +0200

    fetching data in HomePage component implemented

--- client/src/components/Home/HomePage.js
+++ client/src/components/Home/HomePage.js
@@ -1,4 +1,25 @@
+import axios from "axios";
+import { useState, useEffect } from "react"
+
+// import LoadingPage from "../Loading/LoadingPage";
+import MovieCard from "./MovieCard";
+
 export default function HomePage() {
+    const [movies, setMovies] = useState([]);
+    // const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(async () => {
+        console.log("mounted");
+        try {
+            let { data } = await axios.get("http://localhost:8800/api/movies");
+            console.log(data);
+            // setIsLoading(false);
+            setMovies(data);
+            console.log(movies);
+        } catch (err) {
+            console.log(err);
+        }
+    }, []);
 
     return (
         <>
@@ -9,24 +30,7 @@ export default function HomePage() {
                     <h2 className="trending-headline">Trending Now</h2>
 
                     <div className="movies-container">
-                        <div className="movie-card-container">
-
-                            <div className="image-container">
-                                <img src="" alt="img" />
-                            </div>
-
-                            <h2 className="movie-headline">LOREM IPSUM DOLAR SIT</h2>
-                            <p className="movie-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio illo quidem atque omnis, suscipit rerum sunt quod aperiam debitis id.</p>
-
-                            <div className="rating-container">
-                                <p>Rating:</p>
-                                <i className="far fa-star"></i>
-                                <i className="far fa-star"></i>
-                                <i className="far fa-star"></i>
-                                <i className="far fa-star"></i>
-                                <i className="far fa-star"></i>
-                            </div>
-                        </div>
+                        {movies.map(x => <MovieCard key={x._id} movie={x} />)}
                     </div>
                 </div>
 
@@ -59,9 +63,10 @@ export default function HomePage() {
                 </div>
             </div>
 
-            <div className="register-bgr-img-container">
-                <img className="register-bgr-img" src="registerBGR.png" alt="" />
-            </div>
+            <div className="bgr-container"></div>
+            {/* <div className="register-bgr-img-container">
+                    <img className="register-bgr-img" src="registerBGR.png" alt="" />
+                </div> */}
         </>
     )
 }
