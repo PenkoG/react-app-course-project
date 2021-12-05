988b284b13d1565a6983f23483fbe54651983487
PenkoG <81184142+PenkoG@users.noreply.github.com>
2021-12-05 19:12:13 +0200

    ui updates

--- client/src/components/Create/CreatePage.js
+++ client/src/components/Create/CreatePage.js
@@ -27,24 +27,22 @@ export default function CreatePage() {
 
     return (
         <>
-            <div className="register-container">
-                <div className="form-container">
-                    <h2>CREATE MOVIE</h2>
-                    <form className="register-form" onSubmit={onCreateHandler}>
-                        <input className="input" name="title" type="text" placeholder="Movie title:" />
-                        <input className="input" name="description" type="text" placeholder="Description:" />
-                        <input className="input" name="imgUrl" type="text" placeholder="imgUrl:" />
-                        <input className="input" name="trailerURL" type="text" placeholder="trailerURL:" />
-                        <input className="input" name="year" type="text" placeholder="year:" />
-                        <input className="input" name="genre" type="text" placeholder="genre:" />
-                        <input className="input" name="duration" type="text" placeholder="duration:" />
-                        <button className="register-button" >CREATE</button>
-                    </form>
+            <div className="bgr-container">
+                <div className="register-container">
+                    <div className="form-container">
+                        <h2>CREATE MOVIE</h2>
+                        <form className="register-form" onSubmit={onCreateHandler}>
+                            <input className="input" name="title" type="text" placeholder="Movie title:" />
+                            <input className="input" name="description" type="text" placeholder="Description:" />
+                            <input className="input" name="imgUrl" type="text" placeholder="imgUrl:" />
+                            <input className="input" name="trailerURL" type="text" placeholder="trailerURL:" />
+                            <input className="input" name="year" type="text" placeholder="year:" />
+                            <input className="input" name="genre" type="text" placeholder="genre:" />
+                            <input className="input" name="duration" type="text" placeholder="duration:" />
+                            <button className="register-button" >CREATE</button>
+                        </form>
+                    </div>
                 </div>
-            </div>
-
-            <div className="register-bgr-img-container">
-                <img className="register-bgr-img" src="registerBGR.png" alt="" />
             </div>
         </>
     )
