6b2160b837c269f0ed510706f1fd6f07281f5f99
PenkoG <81184142+PenkoG@users.noreply.github.com>
2021-12-05 16:38:49 +0200

    link corrections

--- client/src/components/Details/SideNav.js
+++ client/src/components/Details/SideNav.js
@@ -1,14 +1,19 @@
 import { NavLink } from "react-router-dom";
 
 export default function SideNav() {
+
+    let movieId = sessionStorage.getItem("movie-id");
+    let trailerLink = `/details/${movieId}`;
+    let overviewLink = `/details/${movieId}/overview`;
+
     return (
         <div className="side-nav-container">
             <ul className="side-nav">
                 <li className="side-nav-trailer" >
-                    <NavLink to="/details" className="side-nav-trailer">trailer</NavLink>
+                    <NavLink to={trailerLink} className="side-nav-trailer">trailer</NavLink>
                 </li>
                 <li className="side-nav-overview" >
-                    <NavLink to="/details/overview" className="side-nav-overview">overview</NavLink>
+                    <NavLink to={overviewLink} className="side-nav-overview">overview</NavLink>
                 </li>
             </ul>
         </div>
