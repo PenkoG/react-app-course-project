07e190053e6394637d4b03863fa602c27889e3db
PenkoG <81184142+PenkoG@users.noreply.github.com>
2021-12-05 19:19:40 +0200

    UpdatePage component logic

--- client/src/components/Update/UpdatePage.js
+++ client/src/components/Update/UpdatePage.js
@@ -1,29 +1,50 @@
+import { useEffect, useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 
 export default function UpdatePage() {
+    const [movie, setMovie] = useState({});
     let navigate = useNavigate();
-    let movie = sessionStorage.getItem("movie-info");
-    console.log(movie.title);
+    let movieId = sessionStorage.getItem("movie-id");
 
     useEffect(() => {
-    }, [])
+        (async () => {
+            console.log("mounted");
+            let { data } = await axios.get(`http://localhost:8800/api/movies/find/${movieId}`)
+            setMovie(data)
+        })();
+    }, []);
+
+    console.log(movie);
+
 
     const onUpdateHandler = async (e) => {
         e.preventDefault();
 
         let formData = new FormData(e.currentTarget);
-        let title = formData.get('title');
-        let description = formData.get('description');
-        let imgUrl = formData.get('imgUrl');
-        let trailerUrl = formData.get('trailerUrl');
-        let year = formData.get('year');
-        let genre = formData.get('genre');
-        let duration = formData.get('duration');
-        console.log(`Form Data: ${formData}`);
-    };
+        const title = formData.get('title');
+        const description = formData.get('description');
+        const imgUrl = formData.get('imgUrl');
+        const videoUrl = formData.get('videoUrl');
+        const year = formData.get('year');
+        const genre = formData.get('genre');
+        const duration = formData.get('duration');
+
+        const data = {
+            title,
+            description,
+            imgUrl,
+            videoUrl,
+            year,
+            genre,
+            duration
+        }
+
+        let result = await axios.put(`http://localhost:8800/api/movies/${movieId}`, data);
+        console.log(result);
+        navigate(`/details/${movieId}/overview`);
+    }
 
 
     return (
@@ -33,13 +54,13 @@ export default function UpdatePage() {
                     <div className="form-container">
                         <h2>UPDATE MOVIE</h2>
                         <form className="register-form" onSubmit={onUpdateHandler}>
-                            <input className="input" name="title" type="text" placeholder="Movie title:" value={movie.title} />
-                            <input className="input" name="description" type="text" placeholder="Description:" value={movie.description} />
-                            <input className="input" name="imgUrl" type="text" placeholder="imgUrl:" value={movie.imgUrl} />
-                            <input className="input" name="trailerURL" type="text" placeholder="trailerURL:" value={movie.trailerURL} />
-                            <input className="input" name="year" type="text" placeholder="year:" value={movie.year} />
-                            <input className="input" name="genre" type="text" placeholder="genre:" value={movie.genre} />
-                            <input className="input" name="duration" type="text" placeholder="duration:" value={movie.duration} />
+                            <input className="input" name="title" type="text" placeholder="Movie title:" defaultValue={movie.title} />
+                            <input className="input" name="description" type="text" placeholder="Description:" defaultValue={movie.description} />
+                            <input className="input" name="imgUrl" type="text" placeholder="imgUrl:" defaultValue={movie.imgUrl} />
+                            <input className="input" name="videoUrl" type="text" placeholder="videoUrl:" defaultValue={movie.videoUrl} />
+                            <input className="input" name="year" type="text" placeholder="year:" defaultValue={movie.year} />
+                            <input className="input" name="genre" type="text" placeholder="genre:" defaultValue={movie.genre} />
+                            <input className="input" name="duration" type="text" placeholder="duration:" defaultValue={movie.duration} />
                             <button className="register-button" >UPDATE</button>
                         </form>
                     </div>
