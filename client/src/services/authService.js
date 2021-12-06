9b657809f56c5be0b7a6ef404fa1da04257251aa
PenkoG <81184142+PenkoG@users.noreply.github.com>
2021-12-06 23:18:55 +0200

    create all auth services

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,25 +1,15 @@
 import axios from "axios";
 
-//// Могат да се изкарат в компонентитете
-
-
-
 export const login = async (username, password) => {
-
     let { data } = await axios.post("http://localhost:8800/api/auth/login", { username, password })
-    // sessionStorage.setItem("user", data.username)
     return data;
 }
 
 export const logout = async () => {
-    let respons = await axios.get("http://localhost:8800/api/auth/logout")
-    // sessionStorage.removeItem("user")
+    return await axios.get("http://localhost:8800/api/auth/logout")
+    sessionStorage.removeItem("user");
 }
 
-export const getUser = () => {
-    return localStorage.getItem('user');
-}
-
-export const isAuthenticated = () => {
-    return Boolean(getUser());
+export const register = async (userData) => {
+    return await axios.post("http://localhost:8800/api/auth/register", userData);
 }
