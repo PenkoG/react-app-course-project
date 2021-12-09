e418876b2b00698ab31beb16578c5b7d8989a6a6
PenkoG <81184142+PenkoG@users.noreply.github.com>
2021-12-09 19:58:33 +0200

    Empty movies collection page added

---
+++ client/src/components/MyCollection/NoMoviesPage.js
@@ -0,0 +1,10 @@
+export default function NoMoviesPage() {
+    return (
+
+        <div className="no-movies">
+            <h1 className="no-movies-h1">
+                No movies in your collection yet!
+            </h1>
+        </div>
+    )
+}
