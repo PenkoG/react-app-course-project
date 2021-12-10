ee10bbbee1ea87dc4796c0d4d1bc061e03ed6a9b
PenkoG <81184142+PenkoG@users.noreply.github.com>
2021-12-10 16:54:07 +0200

    404 redesign

--- client/src/components/404/404.js
+++ client/src/components/404/404.js
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom";
 
 import Background from "../Background/Background";
-import styles from "./404.module.css";
+import styles from "./Error.module.css";
 
 export default function Page404() {
     return (
         <>
-            <Link to="/">
-                <button className={styles.btn}> GO TO HOME</button>
-            </Link>
+            <div className={styles.page_container}>
+                <h1 className={styles.fourofour_heading}>404</h1>
+                <p className={styles.fourofour_p}>Page you are looking for is missing</p>
+                <Link className={styles.go_home_link} to="/">
+                    <button className={styles.btn}> GO TO HOME</button>
+                </Link>
+            </div>
             <Background />
         </>
     )
