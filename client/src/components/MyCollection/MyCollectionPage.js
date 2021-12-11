05de3ca36c9061bafc34c5fca7802267c95fe4c0
PenkoG <81184142+PenkoG@users.noreply.github.com>
2021-12-11 16:58:21 +0200

    partial search implementation

--- client/src/components/MyCollection/MyCollectionPage.js
+++ client/src/components/MyCollection/MyCollectionPage.js
@@ -12,12 +12,37 @@ export default function MyCollectionPage() {
     const { user } = useContext(UserContext);
     const [myMovies, setMyMovies] = useState([]);
 
+    let allMovies;
+
     useEffect(() => {
         movieService.getMyMovies(user["_id"])
             .then(res => {
                 setMyMovies(res.data);
             }).catch((err) => console.log(err))
-    })
+    }, [])
+
+    const onChangeHandler = (e) => {
+        e.preventDefault();
+        let search = e.target.value;
+        allMovies = myMovies;
+        let result = [];
+        allMovies.forEach(x => {
+            if (x.title.toLowerCase().includes(search)) {
+                result.push(x);
+            }
+        })
+        if (search.length < 2) {
+            setMyMovies(allMovies);
+        }
+        setMyMovies(result);
+    }
+
+    const onBlurHandler = () => {
+        movieService.getMyMovies(user["_id"])
+            .then(res => {
+                setMyMovies(res.data);
+            }).catch((err) => console.log(err))
+    }
 
     if (!user.isAuthenticated) {
         return (<Navigate to="/login" />)
@@ -34,6 +59,10 @@ export default function MyCollectionPage() {
         <>
             <div className={styles.page}>
                 <div className={styles.my_movies_container}>
+                    <p style={{ color: "white", fontSize: "50px", fontWeight: "800", backgroundColor: "red" }}>
+                        Search
+                    </p>
+                    <input type="text" name="search" placeholder="search" onChange={onChangeHandler} onBlur={onBlurHandler} />
                     <h1 className={styles.headline}>My movies</h1>
                     {areMovies ? moviesCardElement : NoMoviesPage}
                 </div>
