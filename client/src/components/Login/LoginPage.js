220da304864bea3d46134c038a2634320c3e85b4
PenkoG <81184142+PenkoG@users.noreply.github.com>
2021-12-12 17:59:34 +0200

    Login page form validation implemented

--- client/src/components/Login/LoginPage.js
+++ client/src/components/Login/LoginPage.js
@@ -1,14 +1,16 @@
 import { useNavigate, Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 import * as authService from "./../../services/authService";
 import { UserContext } from "../../contexts/UserContext";
-import formExtractor from "../../utils/formProcessing";
+import formExtractor from "../../helpers/formProcessing";
 import Background from "../Background/Background";
 import styles from "./Login.module.css";
+import BasicAlert from "../Alerts/BasicAlert";
 
 export default function LoginPage() {
     const { login } = useContext(UserContext);
+    const [error, setError] = useState('');
     const navigate = useNavigate();
 
     async function onLoginHandler(e) {
@@ -27,13 +29,24 @@ export default function LoginPage() {
             })
     }
 
+    function formValidation(e) {
+        let input = e.currentTarget.value;
+
+        if ((input.length < 3 || input.length > 12)) {
+            setError("Username should be between 3 and 12 charecters!");
+        } else {
+            setError('')
+        }
+    }
+
     return (
         <>
+            {error ? <BasicAlert message={error} /> : ""}
             <div className={styles.login_container}>
                 <div className={styles.form_container}>
                     <h2>LOGIN</h2>
                     <form method="POST" onSubmit={onLoginHandler}>
-                        <input className={styles.input} type="text" placeholder="Username:" name="username" />
+                        <input className={styles.input} type="text" placeholder="Username:" name="username" onBlur={formValidation} />
                         <input className={styles.input} type="password" placeholder="Password:" name="password" />
 
                         <button className={styles.btn}>LOGIN</button>
