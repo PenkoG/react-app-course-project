315e45615804be18090d8ef32133c9e7eb727312
PenkoG <81184142+PenkoG@users.noreply.github.com>
2021-12-12 17:59:23 +0200

    Register page form validation implemented

--- client/src/components/Register/RegisterPage.js
+++ client/src/components/Register/RegisterPage.js
@@ -1,14 +1,18 @@
 import { useNavigate, Link } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { UserContext } from '../../contexts/UserContext';
 
 import * as authService from '../../services/authService'
-import formExtractor from '../../utils/formProcessing';
-import styles from "./Register.module.css";
+import formExtractor from '../../helpers/formProcessing';
+import BasicAlert from '../Alerts/BasicAlert';
 import Background from "../Background/Background";
+import styles from "./Register.module.css";
+
+const regex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 export default function RegisterPage() {
     const navigate = useNavigate();
+    const [error, setError] = useState('');
     const { login } = useContext(UserContext);
 
     function onRegisterHandler(e) {
@@ -29,20 +33,44 @@ export default function RegisterPage() {
                 login(res.data);
                 navigate("/");
             }).catch((err) => {
-                alert(`User: ${userData.username} already registered. If that's you, go on and try to login instead.`)
+                alert(`User: ${userData.username} is already registered.`)
                 console.log(err);
             })
     };
 
+    function formValidation(e) {
+        let input = e.currentTarget.value;
+        let inputType = e.currentTarget.name;
+
+        if (inputType === "username" && (input.length < 3 || input.length > 12)) {
+            setError("Username should be between 3 and 12 charecters!");
+        } else {
+            setError('')
+        }
+
+        if (inputType === "name" && (input.length < 3 || input.length > 12)) {
+            setError("Name should be between 3 and 12 charecters!");
+        } else if (inputType === "name" && (input.length >= 3 || input.length <= 12)) {
+            setError('')
+        }
+
+        if (inputType === "email" && (!input.match(regex))) {
+            setError("Invalid email!");
+        } else if (inputType === "email" && (input.match(regex))) {
+            setError('')
+        }
+    }
+
     return (
         <>
+            {error ? <BasicAlert message={error} /> : ''}
             <div className={styles.register_container}>
                 <div className={styles.form_container}>
                     <h2>REGISTER</h2>
                     <form method="POST" onSubmit={onRegisterHandler}>
-                        <input className={styles.input} name="username" type="text" placeholder="Username:" />
-                        <input className={styles.input} name="name" type="text" placeholder="Name:" />
-                        <input className={styles.input} name="email" type="text" placeholder="Email:" />
+                        <input className={styles.input} name="username" type="text" placeholder="Username:" onChange={formValidation} />
+                        <input className={styles.input} name="name" type="text" placeholder="Name:" onChange={formValidation} />
+                        <input className={styles.input} name="email" type="text" placeholder="Email:" onChange={formValidation} />
                         <input className={styles.input} name="password" type="password" placeholder="Password:" />
                         <input className={styles.input} name="rePassword" type="password" placeholder="Repeat password:" />
                         <button className={styles.btn} >REGISTER</button>
